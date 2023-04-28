a68c2657f93fd47c52541370a1105689b6a8048f
Erickbci <batista.erick07@hotmail.com>
2023-04-27 23:14:29 -0300

    Camada modelo com TDD

--- transactions/model.js
+++ transactions/model.js
@@ -1,6 +1,8 @@
 import admin from 'firebase-admin';
 import { TransactionRepository } from './repository.js';
 import { UserNotInformedError } from './errors/user-not-informed.error.js';
+import { TransactionUidNotInformedError } from './errors/transaction-uid-not-informed-error.js';
+import { TransactionNotFoundError } from './errors/transaction-not-found.error.js';
 
 //Contém a lógica de negócio
 
@@ -25,5 +27,23 @@ export class Transaction {
         }
 
         return this.#repository.findByUserUid(this.user.uid);
+    }
+
+    findByUid() {
+        if (!this.uid) {
+            return Promise.reject(new TransactionUidNotInformedError());
+        }
+
+        return this.#repository.findByUid(this.uid).then(transactionDb => {
+            if (!transactionDb) {
+                return Promise.reject(new TransactionNotFoundError());
+            }
+            this.date = transactionDb.date;
+            this.description = transactionDb.description;
+            this.money = transactionDb.money;
+            this.transactionType = transactionDb.transactionType;
+            this.type = transactionDb.type;
+            this.user = transactionDb.user;
+        })
     }
 }
