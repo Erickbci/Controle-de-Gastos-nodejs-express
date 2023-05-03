c5e92adeba4caf9ad87432d32361711ec4b2019e
Erickbci <batista.erick07@hotmail.com>
2023-05-03 20:44:12 -0300

    Remover transação - Camada Model

--- transactions/model.js
+++ transactions/model.js
@@ -77,4 +77,11 @@ export class Transaction {
             return this.#repository.update(this);
         })
     }
+
+    delete() {
+        return this.findByUid().then(() => {
+            return this.#repository.delete(this);
+        })
+        
+    }
 }
